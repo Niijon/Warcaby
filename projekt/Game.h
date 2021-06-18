5f9795e20e13edc24005dcb64048c0a67d100140
Niijon <niijon6@gmail.com>
2021-06-18 10:22:58 +0200

    Create Game.h

---
+++ projekt/Game.h
@@ -0,0 +1,14 @@
+#ifndef GAME_H
+#define GAME_H
+#include <iostream>
+#include <time.h>
+#include <fstream>
+#include <chrono>
+#include "board.h"
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+void Gameloop();
+
+
+#endif // GAME_H
